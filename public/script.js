c6302dd145b714f6fb55180606cb15359b9c3486
chandrakanth a g <chandrakanthag14@gmail.com>
2026-04-30 14:08:19 +0530

    Second push

---
+++ public/script.js
@@ -0,0 +1,40 @@
+document.getElementById("F").addEventListener("submit",async function(event){
+    event.preventDefault();
+    let name = document.getElementById("name").value.trim();
+    let number = Number(document.getElementById("number").value.trim());
+    const res = await fetch("/save", {
+
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ name, number })
+    });
+    const data = await res.json();
+    if (data.message)
+    {
+        document.getElementById("result").style.color = "green";
+        result.textContent = "Registration Successful!";
+    }
+});
+
+async function searchUser() {
+    document.getElementById("result").style.color = "red";
+    let number = Number(document.getElementById("searchNumber").value);
+
+    const res = await fetch("/search", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ number })
+    });
+
+    const data = await res.json();
+
+    if (data.name) {
+        document.getElementById("result").innerText = data.name;
+    } else {
+        document.getElementById("result").innerText = data.message;
+    }
+}
---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/student")
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err));
+
+const userS = new mongoose.Schema(
+    {
+        name : String,
+        number : Number
+    });
+const User = mongoose.model("User", userS);
+
+app.post("/save", async(req, res)=> {
+    try{
+        const user = new User({
+            name : req.body.name,
+            number : req.body.number
+        });
+    
+        await user.save();
+
+        res.json({ message: "Saved" });
+
+    }catch(err){
+        res.json({ message: "Error" });
+    }
+
+});
+
+
+
+
+app.post("/search", async (req, res) => {
+    try {
+        const number = req.body.number;
+
+        const user = await User.findOne({ number: number });
+
+        if (!user) {
+            return res.json({ message: "User not found" });
+        }
+
+        res.json({ name: user.name });
+
+    } catch (err) {
+        res.json({ message: "Error searching" });
+    }
+});
+
+
+
+app.listen(3001,"0.0.0.0", () => {
+    console.log("Server running on port 3001");
+});
