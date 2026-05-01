a427933d7a79f7ce65a52a9e7f4fa68436294732
chandrakanth a g <chandrakanthag14@gmail.com>
2026-05-01 14:40:01 +0530

    .env added_OG_2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("process.env.MONGO_URI")
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("DB Connected"))
 .catch(err => console.log(err));
 
